125e86cae857cb20ec054b3720a6b1c8dfcfd022
Mateusz Staniaszek <mateusz.staniaszek1998@gmail.com>
2025-12-17 14:53:51 +0100

    fix: update test_openmercato to reflect company creation instead of customer creation

--- examples/test_openmercato.py
+++ examples/test_openmercato.py
@@ -21,14 +21,13 @@ def test_open_mercato_customer_creation(page: Page) -> None:
     companies_xpath = "/html/body/div[2]/div[1]/div/aside/div/div[2]/nav/div[1]/div[1]/a[1]"
     page.locator(f"xpath={companies_xpath}").click()
 
-    new_customer_button_xpath = "/html/body/div[2]/div[1]/div/div/main/div/div/div/div[1]/div[1]/div[2]/a"
-    page.locator(f"xpath={new_customer_button_xpath}").click()
+    new_company_button_xpath = "/html/body/div[2]/div[1]/div/div/main/div/div/div/div[1]/div[1]/div[2]/a"
+    page.locator(f"xpath={new_company_button_xpath}").click()
 
-    fillin_customer_name_xpath = "/html/body/div[2]/div[1]/div/div/main/div/div/div/div[2]/form/div[1]/div[1]/div[1]/div[2]/div/div[1]/input"
-    page.locator(f"xpath={fillin_customer_name_xpath}").fill("Test Customer")
-
-    create_new_customer_xpath="/html/body/div[2]/div[1]/div/div/main/div/div/div/div[1]/div[2]/button"
-    page.locator(f"xpath={create_new_customer_xpath}").click()
+    fillin_company_name_xpath = "/html/body/div[2]/div[1]/div/div/main/div/div/div/div[2]/form/div[1]/div[1]/div[1]/div[2]/div/div[1]/input"
+    page.locator(f"xpath={fillin_company_name_xpath}").fill("Test Company")
+    create_new_company_xpath="/html/body/div[2]/div[1]/div/div/main/div/div/div/div[1]/div[2]/button"
+    page.locator(f"xpath={create_new_company_xpath}").click()
     
     operation_successfull_xpath = "/html/body/div[2]/div[1]/div/div/main/div[1]/div/span[1]"
     expect(page.locator(f"xpath={operation_successfull_xpath}")).to_contain_text("Last operation:")
